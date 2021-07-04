5d466765b007a7e47d00cc90e62d6e866c10739e
TakuoIshida <archersparadox.100@gmail.com>
2021-07-04 16:36:20 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Express from "express";
+import { json, urlencoded } from "body-parser";
+import "reflect-metadata"
+import cors from 'cors'
+import http from 'http'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log(process.env.PORT);
+
+const app: Express.Express = Express();
+app.use(cors({ credentials: true, origin: true }))
+app.use(json())
+app.use(urlencoded({ extended: true }))
+
+const port = 3000;
+
+app.get('/', (req: Express.Request, res: Express.Response) => res.json({
+  msg: "hogehoge",
+  status: 200
+}))
+
+http.createServer(app).listen(3000, () => {
+  console.log(`Example app listening on port ${port}! Access to http://localhost:${port}`)
+})

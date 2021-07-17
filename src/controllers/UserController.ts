a31adb3cb19d589ba216aff68c4281614710dfb0
TakuoIshida <archersparadox.100@gmail.com>
2021-07-17 13:26:56 +0900

    コネクション方法変更、検索処理追加

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,14 @@
 import Express from "express";
 import * as Utils from "../utils/user";
+import { createConnection } from "typeorm";
+
 export class UserContoroller {
   async createUser(
     req: Express.Request
     // res: Express.Response
     // next: Express.NextFunction
   ): Promise<void> {
+    const con = await createConnection("default");
     try {
       const newUser = {
         name: req.body.name,
@@ -13,9 +16,37 @@ export class UserContoroller {
       };
       const result = await Utils.createUser(newUser);
       console.log(result);
-      // await con.close();
     } catch (error) {
       console.log(error.message);
     }
+    con.close();
+  }
+
+  async updateUser(
+    req: Express.Request
+    // res: Express.Response
+    // next: Express.NextFunction
+  ): Promise<void> {
+    const userId = Number(req.body.id);
+    const con = await createConnection("default");
+
+    const finedUser = await Utils.findById(userId);
+    console.log("finedUser");
+    console.log(finedUser);
+
+    if (!finedUser) throw new Error("User not found");
+    try {
+      const newUser = {
+        id: finedUser.id,
+        name: req.body.name,
+        todoList: req.body.todos,
+      };
+      const result = await Utils.updateUser(newUser);
+      console.log(result);
+      con.close();
+    } catch (error) {
+      console.log(error.message);
+    }
+    con.close();
   }
 }
--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,13 +1,39 @@
 import { DeepPartial } from "typeorm";
 import { IUser } from "../contracts/entity/IUser";
 import { User } from "../entity/user";
-import { createConnection } from "typeorm";
+import { getRepository } from "typeorm";
 
 // userのCRUD処理を記載します
+/**
+ * User新規作成
+ * @param newUser
+ * @returns
+ */
 export async function createUser(newUser: DeepPartial<User>): Promise<IUser> {
-  const con = await createConnection("default");
-  const repository = con.getRepository(User);
+  const repository = getRepository(User);
   const createdUser = repository.create(newUser);
   const result = await repository.save(createdUser);
+  return result;
+}
+
+/**
+ * User更新
+ * @param User updateしたいUser
+ * @returns
+ */
+export async function updateUser(user: IUser): Promise<IUser> {
+  const repository = getRepository(User);
+  const result = await repository.save(user);
+  return result;
+}
+
+/**
+ * 指定のUserIdにマッチする、１件取得します
+ * @param user
+ * @returns
+ */
+export async function findById(id: number): Promise<IUser | undefined> {
+  const repository = getRepository(User);
+  const result = await repository.findOne(id);
   return result;
 }

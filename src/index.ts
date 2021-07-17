a9d0308c97da7737d06a03c86e2f79cc278cdcbd
TakuoIshida <archersparadox.100@gmail.com>
2021-07-18 08:40:20 +0900

    不要なテスト処理の削除

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { json, urlencoded } from "body-parser";
 import "reflect-metadata";
 import cors from "cors";
 import dotenv from "dotenv";
+import "./extension/response.extension";
 dotenv.config();
 
 const app: Express.Express = Express();
@@ -11,18 +12,10 @@ app.use(json());
 
 app.use(urlencoded({ extended: true }));
 
-import test from "./routes/test";
-app.use(test);
 import todo from "./routes/todo";
 app.use(todo);
 import user from "./routes/user";
 app.use(user);
-app.get("/", (req: Express.Request, res: Express.Response) =>
-  res.json({
-    msg: "hogehoge",
-    status: 200,
-  })
-);
 
 app.listen(process.env.PORT, () => {
   console.log(`Access to http://localhost:${process.env.PORT}`);

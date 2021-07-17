2fd6811fd6e5b28f75fd584964a2da2e5aeaf76f
TakuoIshida <archersparadox.100@gmail.com>
2021-07-17 13:48:48 +0900

    router/＊＊のリファクタ

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -9,9 +9,6 @@ router.post(
     res.json({
       msg: "ok",
       status: 200,
-      data: {
-        hello: "save todo",
-      },
     });
   }
 );
@@ -19,12 +16,10 @@ router.post(
   "/update/todo",
   (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     // TODO: todoを更新する
+    // TODO: OK, NGレスポンスの共通関数作成
     res.json({
       msg: "ok",
       status: 200,
-      data: {
-        hello: "update todo",
-      },
     });
   }
 );
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,11 +2,11 @@ import Express from "express";
 import { UserContoroller } from "../controllers/UserController";
 const router = Express.Router();
 
-router.post(
-  "/save/user",
+// TODO: validator
+router.get(
+  "/users",
   async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-    const user = new UserContoroller();
-    await user.createUser(req);
+    await new UserContoroller().getUsers(req);
     res.json({
       msg: "ok",
       status: 200,
@@ -15,10 +15,20 @@ router.post(
 );
 
 router.post(
-  "/update/user",
+  "/user/create",
   async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-    const user = new UserContoroller();
-    await user.updateUser(req);
+    await new UserContoroller().createUser(req);
+    res.json({
+      msg: "ok",
+      status: 200,
+    });
+  }
+);
+
+router.post(
+  "/user/update",
+  async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+    await new UserContoroller().updateUser(req);
     res.json({
       msg: "ok",
       status: 200,
